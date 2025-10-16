fa704f24b8e83e0e4d01fab5e6c3e372077bd011
Randy Brown <thatrandybrown@gmail.com>
2025-10-15 20:50:04 -0500

    encode to bytes instead of base64

--- app/index.tsx
+++ app/index.tsx
@@ -39,9 +39,9 @@ export default function Index() {
     const image = canvasRef.current?.makeImageSnapshot();
     if (image) {
       const file = new File(Paths.cache, 'pixel-art.png');
-      const base64 = image.encodeToBase64();
-      file.write(base64);
-      shareAsync(file.uri).then(console.log).catch(console.log);
+      const encodedImage = image.encodeToBytes();
+      await file.write(encodedImage);
+      await shareAsync(file.uri);
     }
   };
 
